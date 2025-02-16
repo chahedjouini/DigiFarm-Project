32d3cace5f5cefc724fb35928ff4f281ad3cb184
Yacineeeen <mohamedyassine.abidi@esprit.tn>
2025-02-16 21:14:35 +0100

    feat : integration gestion etudes + gestion machine (back + front

---
+++ src/Controller/MachineController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Machine;
+use App\Form\MachineType;
+use App\Repository\MachineRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+final class MachineController extends AbstractController
+{
+    #[Route('/machine/', name: 'app_machine_index', methods: ['GET'])]
+    public function index(MachineRepository $machineRepository): Response
+    {
+        return $this->render('frontOfficeMachine/machine/index.html.twig', [
+            'machines' => $machineRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/machine/new', name: 'app_machine_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $machine = new Machine();
+        $form = $this->createForm(MachineType::class, $machine);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($machine);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_machine_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('frontOfficeMachine/machine/new.html.twig', [
+            'machine' => $machine,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/machine/{id}', name: 'app_machine_show', methods: ['GET'])]
+    public function show(Machine $machine): Response
+    {
+        return $this->render('frontOfficeMachine/machine/show.html.twig', [
+            'machine' => $machine,
+        ]);
+    }
+
+    #[Route('/machine/{id}/edit', name: 'app_machine_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Machine $machine, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MachineType::class, $machine);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_machine_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('frontOfficeMachine/machine/edit.html.twig', [
+            'machine' => $machine,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/machine/{id}', name: 'app_machine_delete', methods: ['POST'])]
+    public function delete(Request $request, Machine $machine, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($machine);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_machine_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    // New routes for machine2 directory
+    #[Route('/machine2', name: 'app_machine2_index', methods: ['GET'])]
+    public function indexMachine2(MachineRepository $machineRepository): Response
+    {
+        return $this->render('backOfficeMachine/machine2/index.html.twig', [
+            'machines' => $machineRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/machine2/new', name: 'app_machine2_new', methods: ['GET', 'POST'])]
+    public function newMachine2(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $machine = new Machine();
+        $form = $this->createForm(MachineType::class, $machine);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($machine);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_machine2_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('backOfficeMachine/machine2/new.html.twig', [
+            'machine' => $machine,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/machine2/{id}', name: 'app_machine2_show', methods: ['GET'])]
+    public function showMachine2(Machine $machine): Response
+    {
+        return $this->render('backOfficeMachine/machine2/show.html.twig', [
+            'machine' => $machine,
+        ]);
+    }
+
+    #[Route('/machine2/{id}/edit', name: 'app_machine2_edit', methods: ['GET', 'POST'])]
+    public function editMachine2(Request $request, Machine $machine, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MachineType::class, $machine);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_machine2_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('backOfficeMachine/machine2/edit.html.twig', [
+            'machine' => $machine,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/machine2/{id}', name: 'app_machine2_delete', methods: ['POST'])]
+    public function deleteMachine2(Request $request, Machine $machine, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($machine);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_machine2_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Controller/MaintenanceController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Maintenance;
+use App\Form\MaintenanceType;
+use App\Repository\MaintenanceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+final class MaintenanceController extends AbstractController
+{
+    #[Route('/maintenance/',name: 'app_maintenance_index', methods: ['GET'])]
+    public function index(MaintenanceRepository $maintenanceRepository): Response
+    {
+        return $this->render('frontOfficeMachine/maintenance/index.html.twig', [
+            'maintenances' => $maintenanceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/maintenance/new', name: 'app_maintenance_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $maintenance = new Maintenance();
+        $form = $this->createForm(MaintenanceType::class, $maintenance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($maintenance);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_maintenance_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('frontOfficeMachine/maintenance/new.html.twig', [
+            'maintenance' => $maintenance,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('maintenance/{id}', name: 'app_maintenance_show', methods: ['GET'])]
+    public function show(Maintenance $maintenance): Response
+    {
+        return $this->render('frontOfficeMachine/maintenance/show.html.twig', [
+            'maintenance' => $maintenance,
+        ]);
+    }
+
+    #[Route('/maintenance/{id}/edit', name: 'app_maintenance_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Maintenance $maintenance, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MaintenanceType::class, $maintenance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_maintenance_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('frontOfficeMachine/maintenance/edit.html.twig', [
+            'maintenance' => $maintenance,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/maintenance/{id}', name: 'app_maintenance_delete', methods: ['POST'])]
+    public function delete(Request $request, Maintenance $maintenance, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$maintenance->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($maintenance);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_maintenance_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/maintenance2', name: 'app_maintenance2_index', methods: ['GET'])]
+    public function indexMaintenance2(MaintenanceRepository $maintenanceRepository): Response
+    {
+        return $this->render('backOfficeMachine/maintenance2/index.html.twig', [
+            'maintenances' => $maintenanceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/maintenance2/new', name: 'app_maintenance2_new', methods: ['GET', 'POST'])]
+    public function newMaintenance2(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $maintenance = new Maintenance();
+        $form = $this->createForm(MaintenanceType::class, $maintenance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($maintenance);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_maintenance2_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('backOfficeMachine/maintenance2/new.html.twig', [
+            'maintenance' => $maintenance,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/maintenance2/{id}', name: 'app_maintenance2_show', methods: ['GET'])]
+    public function showMaintenance2(Maintenance $maintenance): Response
+    {
+        return $this->render('backOfficeMachine/maintenance2/show.html.twig', [
+            'maintenance' => $maintenance,
+        ]);
+    }
+
+    #[Route('/maintenance2/{id}/edit', name: 'app_maintenance2_edit', methods: ['GET', 'POST'])]
+    public function editMaintenance2(Request $request, Maintenance $maintenance, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MaintenanceType::class, $maintenance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_maintenance2_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('backOfficeMachine/maintenance2/edit.html.twig', [
+            'maintenance' => $maintenance,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/maintenance2/{id}', name: 'app_maintenance2_delete', methods: ['POST'])]
+    public function deleteMaintenance2(Request $request, Maintenance $maintenance, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$maintenance->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($maintenance);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_maintenance2_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
